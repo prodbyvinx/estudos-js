873f9826e05745f0f55d459db25116d01f378a07
Vinicius Beira <prod.vinx@gmail.com>
2023-05-16 21:28:10 -0300

    novos estudos em JS

---
+++ modulo d/Condições/ex8.js
@@ -0,0 +1,7 @@
+var vel = 100;
+console.log(`A velocidade do seu carro é de ${vel}km/h`);
+
+if (vel > 60) {     //Condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`);
+} 
+    console.log(`Dirija sempre usando cinto de segurança!`);
---
+++ modulo d/Condições/ex9.js
@@ -0,0 +1,9 @@
+var country = 'França';
+
+console.log(`Vivendo em ${country}`);
+
+if (country == 'Brasil', 'Brazil'){     //Condição composta
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
