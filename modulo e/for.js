f57c5645f70e426187e041d7922aba484ee5ec42
Vinicius Beira <prod.vinx@gmail.com>
2023-05-26 20:56:46 -0300

    Aulas sobre repetição e exercício

---
+++ modulo e/for.js
@@ -0,0 +1,21 @@
+/* for (inicio ; teste logico ; incremento){
+        qualquer comando;
+} */
+
+/*var c = 1
+while (c<=10){
+    bloco1
+    c++
+}*/
+
+//  AMBOS SÃO IGUAIS, PORÉM FOR É MAIS PROFISSIONAL;
+
+/*for (var c=1; c<=10; c++){
+    bloco1
+}*/
+
+console.log(`Vai começar...`)
+for (var c = 1; c<= 4; c++) {
+    console.log(c)
+}
+console.log(`FIM!`)
---
+++ modulo e/while.js
@@ -0,0 +1,19 @@
+//Repetições em JS
+/* While(Condição){
+    Bloco1
+};                */
+
+var c = 1
+
+while (c <= 6){
+    console.log("Tudo bem?"+` Passo ${c}`)
+    c++ // C = C+1
+}
+
+/*
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+*/
